00ad45d02c279bdfaeb3106d9bee3d4cc0027580
brianwibowo <sultanapri86@gmail.com>
2024-11-14 19:47:46 +0700

    jinx

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\ComponentsController;
 use App\Http\Controllers\Admin\StatisticsController;
@@ -28,14 +29,12 @@ Route::get('/', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'doLogin'])->name('admin.post_login');
 
 // Route ke dashboard dengan middleware `auth`
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware('auth:admin')->name('admin.dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
 // Route untuk halaman lainnya dengan controller masing-masing
-Route::get('/statistics', [StatisticsController::class, 'index'])->middleware('auth:admin')->name('statistics.index');
-Route::get('/components', [ComponentsController::class, 'index'])->middleware('auth:admin')->name('components.index');
-Route::get('/history', [HistoryController::class, 'index'])->middleware('auth:admin')->name('history.index');
-Route::get('/notifications', [NotificationsController::class, 'index'])->middleware('auth:admin')->name('notifications.index');
-Route::get('/recommender', [RecommenderController::class, 'index'])->middleware('auth:admin')->name('recommender.index');
+Route::get('/statistics', [StatisticsController::class, 'index'])->name('admin.statistics');
+Route::get('/components', [ComponentsController::class, 'index'])->name('admin.components');
+Route::get('/history', [HistoryController::class, 'index'])->name('admin.history');
+Route::get('/notifications', [NotificationsController::class, 'index'])->name('admin.notifications');
+Route::get('/recommender', [RecommenderController::class, 'index'])->name('admin.recommender');
 

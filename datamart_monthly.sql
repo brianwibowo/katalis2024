0a6070487b88729574cf69101314e1f8b1b6df84
brianwibowo <sultanapri86@gmail.com>
2024-11-27 23:22:47 +0700

    uow

---
+++ datamart_monthly.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 27, 2024 at 05:07 PM
+-- Server version: 8.0.36-0ubuntu0.22.04.1
+-- PHP Version: 8.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `greenhousedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datamart_monthly`
+--
+
+CREATE TABLE `datamart_monthly` (
+  `id` bigint NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `node` smallint DEFAULT NULL,
+  `n` double DEFAULT NULL,
+  `p` double DEFAULT NULL,
+  `k` double DEFAULT NULL,
+  `temperature` double DEFAULT NULL,
+  `ph` double DEFAULT NULL,
+  `humidity` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datamart_monthly`
+--
+ALTER TABLE `datamart_monthly`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
